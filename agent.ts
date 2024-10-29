62bf9562ab7a3c0d31897e2c6bacaa3f9772e9fe
krankos <bessaadkhalil@gmail.com>
2024-10-29 14:46:44 +0100

    improved simple agent using graph

--- agent.ts
+++ agent.ts
@@ -8,37 +8,57 @@ dotenv.config();
 
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 import { ChatOpenAI } from "@langchain/openai";
-import { MemorySaver } from "@langchain/langgraph";
-import { HumanMessage } from "@langchain/core/messages";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { HumanMessage } from "@langchain/core/messages"
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
 
 // Define the tools for the agent to use
-const agentTools = [new TavilySearchResults({ maxResults: 3 })];
-const agentModel = new ChatOpenAI({ temperature: 0 });
+const tools = [new TavilySearchResults({ maxResults: 3 })];
+const toolNode = new ToolNode(tools);
 
-// Initialize memory to persist state between graph runs
-const agentCheckpointer = new MemorySaver();
-const agent = createReactAgent({
-  llm: agentModel,
-  tools: agentTools,
-  checkpointSaver: agentCheckpointer,
+const model = new ChatOpenAI({
+  model: "gpt-4o-mini",
+  temperature: 0,
+}).bindTools(tools);
+
+function shouldContinue({messages}: typeof MessagesAnnotation.State){
+    const lastMessage = messages[messages.length - 1];
+
+    // If the LLM makes a tool call, then we route to the "tools" node
+    if (lastMessage.tool_calls.length > 0) {
+      return "tools";
+    }
+    // Otherwise, we stop (reply to the user) using the special "__end__" node
+    return "__end__";
+}
+
+async function callModel(state: typeof MessagesAnnotation.State){
+  const response = await model.invoke(state.messages);
+
+  return { messages: [response]};
+}
+
+// Define a new graph
+const workflow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent")
+  .addNode("tools", toolNode)
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
+
+const app = workflow.compile();
+
+// Use the agent
+const finalState = await app.invoke(
+  {messages: [new HumanMessage("what is the weather in sf")]}
+)
+console.log(finalState.messages[finalState.messages.length - 1].content);
+
+const nextState = await app.invoke({
+  // Including the messages from the previous run gives the LLM context.
+  // This way it knows we're asking about the weather in NY
+  messages: [...finalState.messages, new HumanMessage("what is the weather in ny")]
 });
+console.log(nextState.messages[nextState.messages.length - 1].content);
 
-// Now it's time to use!
-const agentFinalState = await agent.invoke(
-  { messages: [new HumanMessage("what is the current weather in sf")] },
-  { configurable: { thread_id: "42" } }
-);
 
-console.log(
-  agentFinalState.messages[agentFinalState.messages.length - 1].content
-);
-
-const agentNextState = await agent.invoke(
-  { messages: [new HumanMessage("what about ny")] },
-  { configurable: { thread_id: "42" } }
-);
-
-console.log(
-  agentNextState.messages[agentNextState.messages.length - 1].content
-);
